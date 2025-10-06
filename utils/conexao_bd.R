5447cc6e78072571b1b47fb8cd5b64ec97e78be5
rafasfer2 <rafasfer2@gmail.com>
2025-10-06 18:25:35 -0300

    ok

--- utils/conexao_bd.R
+++ utils/conexao_bd.R
@@ -1,11 +1,11 @@
 conectar_bd <- function() {
   DBI::dbConnect(
     RPostgres::Postgres(),
-    dbname   = "postgres",
-    host     = "aws-1-sa-east-1.pooler.supabase.com",
-    port     = 6543,
-    user     = "postgres.dnakyeffnvqcosrjumoa",
+    host = "aws-1-sa-east-1.pooler.supabase.com",
+    port = 5432,
+    dbname = "postgres",
+    user = "postgres.dnakyeffnvqcosrjumoa",
     password = "Rafasfer2@2260",
-    sslmode  = "require"
+    sslmode = "require"
   )
 }
